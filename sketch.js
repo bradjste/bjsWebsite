6edfd4e9b2a82ab2a3deb41cb4c0665801499a8c
Brad Stevenson <42152560+bradjste@users.noreply.github.com>
2018-12-20 23:26:40 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,8 +4,13 @@ var colorPhase;
 var freq1,freq2,freq3,freq4,freq5;
 var count;
 var milTemp;
-var button, artButton, musicButton;
+var splashButton, artButton, musicButton, aboutButton;
 var isSplash = true;
+var artIsSelected = false;
+var musicIsSelected = false;
+var projIsSelected = false;
+var aboutIsSelected = false;
+var mooove = true;
 
 
 function setup() {
@@ -33,35 +38,44 @@ function setup() {
   strokeWeight(60);
   count = 0.0;
 
-  button = createButton('ENTER');
+  splashButton = createButton('ENTER');
+  splashButton.style('border-radius', '20%');
+  splashButton.style('cursor', 'pointer');
+  splashButton.style('-webkit-transition', 'opacity 1s');
+  splashButton.style('transition', 'opacity 1s');
+  splashButton.mousePressed(menuTrans);
+
   artButton = createButton('ART');
-  musicButton = createButton('MUSIC');
   artButton.hide();
-  musicButton.hide();
   artButton.style('opacity', '0');
-  musicButton.style('opacity', '0');
-
-  button.style('border-radius', '20%');
-  button.style('cursor', 'pointer');
-  button.style('-webkit-transition', 'opacity 1s');
-  button.style('transition', 'opacity 1s');
-  button.mousePressed(menuTrans);
-
   artButton.style('border-radius', '20%');
   artButton.style('cursor', 'pointer');
   artButton.style('-webkit-transition', 'opacity 1s');
   artButton.style('transition', 'opacity 1s');
-  //artButton.mousePressed(artTrans);
+  artButton.mousePressed(artTrans);
+
+  musicButton = createButton('MUSIC');
+  musicButton.hide();
+  musicButton.style('opacity', '0');
   musicButton.style('border-radius', '20%');
   musicButton.style('cursor', 'pointer');
   musicButton.style('-webkit-transition', 'opacity 1s');
   musicButton.style('transition', 'opacity 1s');
-  //musicButton.mousePressed(musicTrans);
-  //button.position(windowWidth*0.5-(button.width*0.5), windowHeight*0.5);
-  button.size(windowWidth*0.1,windowWidth*0.05);
-  //button.style('background-color', col);
+  musicButton.mousePressed(musicTrans);
 
-//button.mousePressed(changeBG);
+  projButton = createButton('STUFF');
+  projButton.hide();
+  projButton.style('opacity', '0');
+  projButton.style('border-radius', '20%');
+  projButton.style('cursor', 'pointer');
+  projButton.style('-webkit-transition', 'opacity 1s');
+  projButton.style('transition', 'opacity 1s');
+  projButton.mousePressed(projTrans);
+  //splashButton.position(windowWidth*0.5-(splashButton.width*0.5), windowHeight*0.5);
+  splashButton.size(windowWidth*0.1,windowWidth*0.05);
+  //splashButton.style('background-color', col);
+
+//splashButton.mousePressed(changeBG);
 }
 
 function draw() {
@@ -84,6 +98,7 @@ function draw() {
   var col4 = color((colorPhase+240)%360,100*(mouseX/windowWidth),100);
   var col2 = color(colorPhase,100*(mouseX/windowWidth),100);
   var col3 = color(colorPhase,100,100);
+  var col5 = color((colorPhase+240)%360,100,100);
   stroke(col);
   fill(col2);
   line(mouseX,0,mouseX,windowHeight);
@@ -101,7 +116,7 @@ function draw() {
     ellipse(mouseX+random(40*(mouseX/windowWidth))-20,mouseY+random(40*(mouseX/windowWidth))-20,windowWidth*0.2,windowWidth*0.2);
     textSize(windowWidth*0.02);
     fill(0);
-    text(int(freq1)+' Hz',mouseX,mouseY);
+    text(int(freq1)+' Hz',mouseX+random(40*(mouseX/windowWidth))-20,mouseY+random(40*(mouseX/windowWidth))-20);
     osc1.amp(.2);
     osc2.amp(.2);
     osc3.amp(.2);
@@ -135,28 +150,57 @@ function draw() {
     fill(col4);
     text("interdisciplinary creative",windowWidth*0.5,windowHeight*0.2);
   }
-  button.style('border-color', col3);
-  button.style('color', col3);
-  button.style('background-color', '#000000');
-  button.style('font-size',windowWidth*0.02 + 'px');
-  button.size(windowWidth*0.1,windowWidth*0.05);
-  button.position((windowWidth*0.5)-(button.width*0.5),
-      (windowHeight*0.85)-(button.height*0.5)+(sin(count)*windowHeight*0.02));
+  splashButton.style('border-color', col3);
+  splashButton.style('color', col3);
+  splashButton.style('background-color', '#000000');
+  splashButton.style('font-size',windowWidth*0.02 + 'px');
+  splashButton.size(windowWidth*0.1,windowWidth*0.05);
+  splashButton.position((windowWidth*0.5)-(splashButton.width*0.5),
+      (windowHeight*0.85)-(splashButton.height*0.5)+(sin(count)*windowHeight*0.02));
 
-  artButton.style('border-color', col3);
-  artButton.style('color', col3);
-  artButton.style('background-color', '#000000');
+  if (!artIsSelected) {
+      artButton.style('border-color', col3);
+      artButton.style('color', col3);
+      artButton.style('background-color', '#000000');
+  }
+  else {
+    artButton.style('border-color', col5);
+    artButton.style('color',col5);
+  }
   artButton.style('font-size',windowWidth*0.02 + 'px');
   artButton.size(windowWidth*0.1,windowWidth*0.05);
   artButton.position(windowWidth*0.1,windowHeight*0.1);
 
-  musicButton.style('border-color', col3);
-  musicButton.style('color', col3);
-  musicButton.style('background-color', '#000000');
+  if (!musicIsSelected) {
+    musicButton.style('border-color', col3);
+    musicButton.style('color', col3);
+    musicButton.style('background-color', '#000000');
+  }
+  else {
+    musicButton.style('border-color', col5);
+    musicButton.style('color',col5);
+  }
   musicButton.style('font-size',windowWidth*0.02 + 'px');
   musicButton.size(windowWidth*0.1,windowWidth*0.05);
   musicButton.position(windowWidth*0.1+(artButton.width*0.25)+artButton.width,windowHeight*0.1);
 
+  if (!projIsSelected) {
+    projButton.style('border-color', col3);
+    projButton.style('color', col3);
+    projButton.style('background-color', '#000000');
+  }
+  else {
+    projButton.style('border-color', col5);
+    projButton.style('color',col5);
+  }
+  projButton.style('font-size',windowWidth*0.02 + 'px');
+  projButton.size(windowWidth*0.1,windowWidth*0.05);
+  if (mooove){
+    projButton.position(windowWidth-(musicButton.x+musicButton.width),windowHeight*0.1);
+  }
+  else {
+    projButton.position(windowWidth-(artButton.x+artButton.width),windowHeight*0.1);
+  }
   var freq = map(mouseX, 0, width, 40, 500);
   var off = map(mouseY, 0, height, 20, .01);
   freq1 = freq*1;
@@ -185,19 +229,40 @@ function draw() {
 
 function windowResized() {
   resizeCanvas(windowWidth-5, windowHeight-5);
+  background(0);
 }
 
 function menuTrans() {
   isSplash = false;
-  button.style('opacity', '0');
-  //background(0);
+  splashButton.style('opacity', '0');
   artButton.show();
   musicButton.show();
+  projButton.show();
   milTemp = millis();
 }
 
 function loadMenu() {
-  button.hide();
+  splashButton.hide();
   artButton.style('opacity','100');
   musicButton.style('opacity','100');
+  projButton.style('opacity','100');
+}
+
+function artTrans(){
+  artIsSelected = true;
+  musicIsSelected = false;
+  projIsSelected = false;
+}
+
+function musicTrans(){
+  musicIsSelected = true;
+  artIsSelected = false;
+  projIsSelected = false;
+}
+
+function projTrans(){
+  projIsSelected = true;
+  artIsSelected = false;
+  musicIsSelected = false;
+  mooove = !mooove;
 }
